d272b17c3ee22e29e873ec1acfcf4a3706ca03fb
mkeyhanim <MkeyhaniDeve@gmail.com>
2025-01-01 20:39:15 +0330

    create static header table

---
+++ dynamic-schedule-grid/src/components/TheadTable.tsx
@@ -0,0 +1,34 @@
+const TheadTable = () => {
+  return (
+    <thead>
+      <tr className="text-slate-500 border-b border-slate-300 bg-slate-50">
+        <th className="p1">
+          <p className="text-sm leading-none font-normal">Time</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Sunday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Monday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Tuesday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Wednesday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Thursday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Friday</p>
+        </th>
+        <th className="p-4">
+          <p className="text-sm leading-none font-normal">Saturday</p>
+        </th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TheadTable;
